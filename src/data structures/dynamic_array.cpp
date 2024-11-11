ca7ff8774996f27220cf0572fec5788a035eec59
DEVUCP <OMHN05@protonmail.com>
2024-11-11 12:08:32 +0200

    feat: Add 'clear()' method for dynamic array

--- src/data structures/dynamic_array.cpp
+++ src/data structures/dynamic_array.cpp
@@ -1,30 +1,31 @@
+#pragma once
 #include <iostream>
 #include <type_traits>
 
 using std::cout;
 
 template <typename T>
-class DynamicArray{
+class DynamicArr{
     private:
         int size;
         int capacity;
-        T* array;
+        T* arr;
 
     public:
 
-        DynamicArray(){
+        DynamicArr(){
             size = 0;
             capacity = 1;
-            array = new T[capacity];
+            arr = new T[capacity];
         }
 
-        T operator[](int index) { // Overloads [] operator for element retrieval
-        return this->array[index];
+        T& operator[](int index) { // Overloads [] operator for element retrieval
+        return this->arr[index];
         }   
 
-        friend std::ostream& operator<<(std::ostream& os, const DynamicArray<T>& arr){ // Overloads the ostream << operator for full array output
+        friend std::ostream& operator<<(std::ostream& os, const DynamicArr<T>& arr){ // Overloads the ostream << operator for full arr output
             for(int i = 0; i < arr.size; i++){
-                os << arr.array[i] << " ";
+                os << arr.arr[i] << " ";
             }
 
             return os;
@@ -32,39 +33,39 @@ class DynamicArray{
 
         void shrink(){
             capacity /= 2;
-            T* new_array = new T[capacity];
+            T* new_arr = new T[capacity];
 
-            for(int i = 0; i < size; i++){ // Copies old array to new array
-                new_array[i] = array[i];
+            for(int i = 0; i < size; i++){ // Copies old arr to new arr
+                new_arr[i] = arr[i];
             }
 
-            delete [] array;
-            array = new_array;
+            delete [] arr;
+            arr = new_arr;
         }
 
         void grow(){
             capacity *= 2;
-            T* new_array = new T[capacity];
+            T* new_arr = new T[capacity];
 
-            for(int i = 0; i < size; i++){ // Copies old array to new array
-                new_array[i] = array[i];
+            for(int i = 0; i < size; i++){ // Copies old arr to new arr
+                new_arr[i] = arr[i];
             }
 
-            delete [] array;
-            array = new_array;
+            delete [] arr;
+            arr = new_arr;
         }
 
         void append(const T& elem){
             if(size == capacity)
                 grow();
-            array[size++] = elem;
+            arr[size++] = elem;
         }
 
         T peek(int index){
             if(index < 0 || index > size-1)
                 throw std::out_of_range("Index out of range");
             
-            return array[index];
+            return arr[index];
         }
         
         void insert(const T& elem, int index){
@@ -78,10 +79,10 @@ class DynamicArray{
                 grow();
 
             for(int i = size;i > index;i--){
-                array[i] = array[i-1];
+                arr[i] = arr[i-1];
             }
 
-            array[index] = elem;
+            arr[index] = elem;
             size++;
         }
 
@@ -95,9 +96,9 @@ class DynamicArray{
             }
 
             int index = size-1;
-            T data = array[index];   
+            T data = arr[index];   
 
-            array[index] = T();
+            arr[index] = T();
 
             size--;
             return data;
@@ -112,13 +113,13 @@ class DynamicArray{
                 shrink();
             }
 
-            T data = array[index];   
+            T data = arr[index];   
 
             for(int i = index; i < size-1; i++){
-                array[i] = array[i+1];
+                arr[i] = arr[i+1];
             }
 
-            array[size-1] = T();
+            arr[size-1] = T();
             size--;
             return data;
 
@@ -126,10 +127,10 @@ class DynamicArray{
 
         void print(){
             if(!size)
-                throw std::out_of_range("Array is empty");
+                throw std::out_of_range("arr is empty");
             cout << "[ ";
             for(int i = 0; i < size; i++){
-                cout << array[i] << (i < size-1 ? ", " : "");
+                cout << arr[i] << (i < size-1 ? ", " : "");
             }
             cout << " ]" << std::endl;
         }
@@ -146,8 +147,8 @@ class DynamicArray{
 
                 for (int j = 0; j < n - i - 1; ++j) {
 
-                    if (this->array[j] > this->array[j + 1]) {
-                        std::swap(this->array[j], this->array[j + 1]);
+                    if (this->arr[j] > this->arr[j + 1]) {
+                        std::swap(this->arr[j], this->arr[j + 1]);
                         swapped = true;
                     }
                 }
@@ -162,7 +163,7 @@ class DynamicArray{
             int end = this->size - 1;
 
             while (beg < end) { // swap alternating
-                std::swap(this->array[beg++], this->array[end--]);
+                std::swap(this->arr[beg++], this->arr[end--]);
             }
         }
         
@@ -171,11 +172,11 @@ class DynamicArray{
             int count = 0;
             for (int j = 0; j < this->size; j++) {
 
-                if (this->array[j] > 1) { 
+                if (this->arr[j] > 1) { 
                     bool prime = true; 
 
-                    for (int i = 2; i * i <= this->array[j]; ++i) {
-                        if (this->array[j] % i == 0) {
+                    for (int i = 2; i * i <= this->arr[j]; ++i) {
+                        if (this->arr[j] % i == 0) {
                             prime = false;
                             break;
                         }
@@ -192,17 +193,20 @@ class DynamicArray{
             static_assert(std::is_same<T, int>::value, "countEven() can only be used with int type.");
             int count = 0;
             for (int i = 0; i < this->size; i++) {
-                if (this->array[i] % 2 == 0) {
+                if (this->arr[i] % 2 == 0) {
                     count++;
                 }
             }
             return count;
         }
-
-
+        void clear(){
+            size = 0;
+            delete [] arr;
+            arr = new T[capacity];
+        }
         // I would add countPrime() and countEven() methods, but those are int exclusive
-        ~DynamicArray(){
-            delete [] array;
+        ~DynamicArr(){
+            delete [] arr;
         }
 };
 
